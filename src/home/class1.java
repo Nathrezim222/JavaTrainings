d824cd56dae4bd6c0c3dbff9ecbbb6578d231fad
Nathrezim222 <nathrezim217@gmail.com>
2015-09-06 19:54:25 +0300

    About commit

---
+++ src/home/class1.java
@@ -0,0 +1,11 @@
+package home;
+
+/**
+ * Created by User on 06.09.2015.
+ */
+public class class1 {
+    public static void main(String[] args) {
+        System.out.println("Hello java");
+    }
+
+}
